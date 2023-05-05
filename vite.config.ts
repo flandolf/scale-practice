be82e652f2f9a879e7f5ecc5b6d26c769230ec0f
flandy <dumpyee09@gmail.com>
2023-05-05 19:16:44 +1000

    Remove compression, doesn't help

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
     react(),
-    viteCompression({
-      filter: /\.(js|mjs|json|css|html)$/i,
-      algorithm: "brotliCompress",
-    }),
   ],
   build: {
     commonjsOptions: {
